8af0d9928cc594d532dc0617971e38415d647714
MentalBlood <neceporenkostepan@gmail.com>
2020-11-24 14:43:45 +0300

    fixed requestJsonGenerator

--- requestJsonGenerator.py
+++ requestJsonGenerator.py
@@ -2,13 +2,20 @@ import json
 import base64
 import argparse
 
+defaultImagesPaths = [
+	'fashion-mnist_png\\test\\1\\2.png',
+	'mnist_png\\testing\\2\\1.png',
+	'mnist_png\\testing\\4\\4.png',
+	'fashion-mnist_png\\test\\5\\8.png'
+]
+
 parser = argparse.ArgumentParser(description='Generate JSON for request')
-parser.add_argument('--dataset', type=str, nargs='?', required=True,
+parser.add_argument('--dataset', type=str, nargs='?',
 					choices=['MNIST', 'FashionMNIST'],
-					default='MNIST',
                     help='dataset model should be trained on')
-parser.add_argument('--images', metavar='image', type=str, required=True, nargs='+',
-                    help='paths to files to send in request')
+parser.add_argument('--images', metavar='image', type=str, nargs='+',
+                    help='paths to files to send in request',
+                    default=defaultImagesPaths)
 args = parser.parse_args()
 
 imgPaths = args.images
@@ -20,5 +27,9 @@ def readImage(path):
 		result = base64.encodebytes(imageBytes).decode("utf-8")
 	return result
 img = list(map(readImage, imgPaths))
-jsonString = json.dumps({'images': img, 'dataset': netName})
+jsonString = None
+if netName:
+	jsonString = json.dumps({'images': img, 'dataset': netName})
+else:
+	jsonString = json.dumps({'images': img})
 print(jsonString)
